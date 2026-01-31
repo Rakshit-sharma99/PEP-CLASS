891994c97f9c7aca38b48b44689e92b6baf9f1ff
Rakshit sharma <150055691+Rakshit-9999@users.noreply.github.com>
2026-01-31 09:42:12 +0530

    Day 8 performing linked list

---
+++ DAY 8/Linked.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Linked {
+    public static void main(String[] args) {
+        LinkedList<Character> list = new LinkedList<>();
+        list.add('S');
+        System.out.println(list);
+    }
+}
